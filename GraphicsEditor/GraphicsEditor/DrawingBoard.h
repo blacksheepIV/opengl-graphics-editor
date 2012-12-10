ec69a18fcb699b8c3044439306e699c14cc4f855
nsatvik <nsatvik@gmail.com>
2012-12-10 14:49:42 +0000

    Initial Import

---
+++ GraphicsEditor/GraphicsEditor/DrawingBoard.h
@@ -0,0 +1,4 @@
+/*
+ *This is the main drawing board class.
+ * supposed to contain the object of other classes like toolbar, colorpanel, menubar, and the drawing canvas on which everything is drawn.
+ */
