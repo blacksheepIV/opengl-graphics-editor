0996b2506c8332af31378268123a5b99c0bac290
nsatvik <nsatvik@gmail.com>
2012-12-14 15:00:23 +0000

    In Sync with the Github Version 14/12/2012 20.30!

--- GraphicsEditor/GraphicsEditor/Canvas.h
+++ GraphicsEditor/GraphicsEditor/Canvas.h
@@ -2,19 +2,40 @@
 #define _CANVAS_H
 #include "Coordinates.h"
 #include "Constants.h"
+#include "Tool.h"
+#include<string.h>
+#include "ColorPanel.h"
 #include<gl/glut.h>
+
+//class Tool;
 class Canvas
 {
 private:
 	Coordinates bottom_left;
-	Coordinates top_right;
-	//float ImageData[][]; An array to hold the drawn data same as our my_package.cpp
-
+	Coordinates top_right;	
+	char *fileName;
+	Coordinates firstPoint;			//The first point clicked by the user
+	bool isFirstEntry;
+	inline void readFileName();
+	GLfloat imageData[APPLICATION_WINDOW_HEIGHT][APPLICATION_WINDOW_WIDTH * MULT_FACTOR]; //An array to hold the drawn data same as our my_package.cpp
 public:
+	
+
 	Canvas(float, float , float , float);
 	void drawBoard();
 	bool isClickInside(int , int);
+	void saveToFile();
+	void drawWithTool(Tool *tool, Color color, int x, int y);
+	Coordinates getFirstPoint();	
+	void LoadFromFile();
+	~Canvas();
+	
 };
+Canvas::~Canvas()
+{
+	delete fileName;
+}
+
 Canvas::Canvas(float x1, float y1, float x2, float y2)
 {
 	bottom_left.set(X_AXIS, x1);
@@ -22,11 +43,21 @@ Canvas::Canvas(float x1, float y1, float x2, float y2)
 
 	top_right.set(X_AXIS, x2);
 	top_right.set(Y_AXIS, y2);
+
+	for(int i=0;i<APPLICATION_WINDOW_HEIGHT;i++)
+		for(int j=0;j<(APPLICATION_WINDOW_WIDTH*MULT_FACTOR);j++)
+			imageData[i][j] = 1;
+
+	isFirstEntry = false;
+	fileName = new char[NAMSIZE];
+	fileName[0] = '\0';
+	//glReadPixels(CANVAS_LEFT, CANVAS_BOTTOM, CANVAS_RIGHT, CANVAS_TOP, GL_RGB, GL_FLOAT, imageData);	
 }
+
 bool Canvas::isClickInside(int x, int y)
 {
 	if((x>=bottom_left.get(X_AXIS))&&(x<=top_right.get(X_AXIS)))
-		return (y>=bottom_left.get(Y_AXIS))&&(x<=top_right.get(Y_AXIS));
+		return (y>=bottom_left.get(Y_AXIS))&&(y<=top_right.get(Y_AXIS));
 	else
 		return false;
 }
@@ -34,6 +65,70 @@ void Canvas::drawBoard()
 {
 	glColor3f(1,1,1);
 	glRectf(this->bottom_left.get(X_AXIS), this->bottom_left.get(Y_AXIS), top_right.get(X_AXIS), top_right.get(Y_AXIS));
+
+	glRasterPos2i(CANVAS_LEFT, CANVAS_BOTTOM);
+	glDrawPixels(CANVAS_RIGHT - CANVAS_LEFT, CANVAS_TOP - CANVAS_BOTTOM, GL_RGB,GL_FLOAT, imageData);
+}
+
+void Canvas::drawWithTool(Tool *tool, Color color, int x, int y)
+{
+	firstPoint.set(X_AXIS, x);
+	firstPoint.set(Y_AXIS, y);
+
+	color.setGLColor();	
+	tool->drawOnCanvas(this, imageData, x, y);			
+
+	glReadPixels(CANVAS_LEFT, CANVAS_BOTTOM, CANVAS_RIGHT - CANVAS_LEFT, CANVAS_TOP - CANVAS_BOTTOM, GL_RGB, GL_FLOAT, imageData);	
+}
+
+Coordinates Canvas::getFirstPoint()
+{
+	return firstPoint;
+}
+
+/*GLfloat Canvas::(*getImageData())[APPLICATION_WINDOW_WIDTH * 3]
+{
+	return imageData;
+}*/
+void Canvas::readFileName()
+{
+	cout<<"Enter the file name"<<endl;
+	cin >> fileName;
+
+}
+void Canvas::LoadFromFile()
+{
+	ifstream ifile;
+	do
+	{
+		readFileName();
+		ifile.open(fileName, ios::in);
+		LOG(fileName);
+		if(ifile.is_open())
+			break;
+		else
+			cout<<"Unable to read the File"<<endl;
+	}while(1);
+	ifile.read((char *)imageData, sizeof(imageData));
+	drawBoard();
+}
+void Canvas::saveToFile()
+{
+	ofstream outFile;
+	if(strlen(fileName)==0)
+	{
+		do
+		{
+			readFileName();
+			outFile.open(fileName, ios::out);
+			if(outFile.is_open())
+			{break;
+			}
+			else
+			 cout<<"File Not Found :P "<<endl;
+		}while(1);
+	}
+	outFile.write((char *) imageData, sizeof(imageData));
 }
 
 #endif
--- GraphicsEditor/GraphicsEditor/application_main.cpp
+++ GraphicsEditor/GraphicsEditor/application_main.cpp
@@ -1,48 +1,94 @@
+#include<iostream>
+#include<fstream>
 #include "Constants.h"
 #include "DrawingBoard.h"
+//#include "Coordinates.h"
+
+//#include "Canvas.h"
+//#include "Tool.h"
 #include<gl/glut.h>
-#include<iostream>
+
 using namespace std;
 
 DrawingBoard drawingBoard;
+int reshapeWidth = APPLICATION_WINDOW_WIDTH, reshapeHeight = APPLICATION_WINDOW_HEIGHT;
+
+
 void display(void);
 void myinit(void);
 void handleMouseEvent(int,int,int,int);
+void handleMouseClickMotion(int x, int y);
+void reshape(int, int);
+void handleKeyPress(unsigned char , int, int);
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(APPLICATION_WINDOW_WIDTH, APPLICATION_WINDOW_HEIGHT); // Add size in constants.h
-	glutInitWindowPosition(10,10);
+	glutInitWindowSize(APPLICATION_WINDOW_WIDTH, APPLICATION_WINDOW_HEIGHT);
+	glutInitWindowPosition(700,100);
 	glutCreateWindow("Jinkchak Paint Package");
 	glutDisplayFunc(display);
+	//glutReshapeFunc(reshape);
+	glutKeyboardFunc(handleKeyPress);
 	glutMouseFunc(handleMouseEvent);
+	glutMotionFunc(handleMouseClickMotion);
 	myinit();
 	glutMainLoop();
 }
+void handleKeyPress(unsigned char key, int x, int y)
+{
+}
 void myinit(void)
 {
+	glEnable(GL_BLEND); //Enable blending.
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //Set blending function.
+
 	glClearColor(0.8784,0.8784,0.8784,0.8784);
 	glColor3f(1,1,1);
 	glPointSize(1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0,499,0,499);
+	gluOrtho2D(0,APPLICATION_WINDOW_WIDTH,0,APPLICATION_WINDOW_HEIGHT);
+
+	glMatrixMode(GL_MODELVIEW);
 }
+
 void handleMouseEvent(int button, int state, int x, int y)
 {
+	float scaleFactorWidth = (float)reshapeWidth / APPLICATION_WINDOW_WIDTH;
+	float scaleFactorHeight = (float)reshapeHeight / APPLICATION_WINDOW_HEIGHT;
+
+	x *= scaleFactorWidth;
+	y = APPLICATION_WINDOW_HEIGHT - y;
+	y *= scaleFactorHeight;
 	drawingBoard.handleMouseClick(button, state, x, y);
-	cout<<"Mouse Click ("<<x<<","<<y<<")"<<endl;
+
+	glutPostRedisplay();
 }
+
+void handleMouseClickMotion(int x, int y)
+{
+	y = APPLICATION_WINDOW_HEIGHT - y;
+	
+	drawingBoard.handleMouseClickMotion(x, y);
+
+	glutPostRedisplay();
+}
+
 void display(void)
 {
-
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0,0,0);
-	drawingBoard.initBoard();
-	glBegin(GL_LINES);
-	glVertex2d(10,10);
-	glVertex2d(30,30);
-	glEnd();
+	glColor3f(1,1,1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	drawingBoard.initBoard();	
 	glFlush();
+}
+
+void reshape(int w, int h)
+{
+	reshapeWidth = w;
+	reshapeHeight = h;
 }
