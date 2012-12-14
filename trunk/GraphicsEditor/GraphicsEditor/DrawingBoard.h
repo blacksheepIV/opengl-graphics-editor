ba33be4221ed6e5232fa91e89ae485a8cba51656
nsatvik@gmail.com <nsatvik@gmail.com@d397f6aa-7d3e-2b7f-dff3-4ad177eba80e>
2012-12-14 15:00:23 +0000

    In Sync with the Github Version 14/12/2012 20.30!

--- trunk/GraphicsEditor/GraphicsEditor/DrawingBoard.h
+++ trunk/GraphicsEditor/GraphicsEditor/DrawingBoard.h
@@ -7,6 +7,7 @@
 #include<gl/glut.h>
 #include<iostream>
 using namespace std;
+
 /*
  *This is the main drawing board class.
  * supposed to contain the object of other classes like toolbar, colorpanel, menubar, and the drawing canvas on which everything is drawn.
@@ -23,9 +24,13 @@ public:
 	DrawingBoard();
 	void initBoard();
 	void handleMouseClick(int, int, int, int);
+	void handleMouseClickMotion(int, int);
 	~DrawingBoard();
 	
+	Canvas *getCanvas();
+	
 };
+
 DrawingBoard::DrawingBoard()
 {
 	canvas_board = new Canvas(CANVAS_LEFT,CANVAS_BOTTOM, CANVAS_RIGHT,CANVAS_TOP);
@@ -33,24 +38,60 @@ DrawingBoard::DrawingBoard()
 	menu_bar = new MenuBar(MENUBAR_LEFT, MENUBAR_BOTTOM, MENUBAR_RIGHT, MENUBAR_TOP);
 	drawingToolBar = new DrawingToolBar( TOOLBAR_LEFT, TOOLBAR_BOTTOM, TOOLBAR_RIGHT, TOOLBAR_TOP);
 }
+
 void DrawingBoard::handleMouseClick(int button, int state, int x, int y)
-{
+{	
 	if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN)
 	{
+
+		LOG("Mouse Click (x,y) = ");
+		LOG(x<<' '<<y);
 		if(canvas_board->isClickInside(x,y))
 		{
-			cout<<"Mouse Click ("<<x<<","<<y<<") - Inside CanvasBoard"<<endl;
+			LOG("  Inside CanvasBoard");		
+			drawingToolBar->getSelectedTool()->start();
+			canvas_board->drawWithTool(drawingToolBar->getSelectedTool(), color_panel->getSelectedColor(), x, y);
 		}
 		if(color_panel->isClickInside(x,y))
 		{
-			cout<<"Mouse Click ("<<x<<","<<y<<") - Inside Color Panel"<<endl;
+			LOG("  Inside Color Panel");
+			color_panel->selectClickedColorFromGrid(x, y);	
+			color_panel->selectClickedColorFromTriangle(x, y);		
 		}
 		if(drawingToolBar->isClickInside(x,y))
 		{
-			cout<<"Mouse Click ("<<x<<","<<y<<") - Inside Drawing Toolbar"<<endl;
+			LOG("  Inside Drawing Toolbar");	
+			drawingToolBar->selectClickedToolFromGrid(x, y);
+		}
+		if(menu_bar->isClickInsideMenu(x,y))
+		{
+			LOG("Inside Menu Bar");
+			menu_bar->performMenuOperation(canvas_board);
 		}
 	}
+
+
+	if(button==GLUT_LEFT_BUTTON && state==GLUT_UP)
+	{
+		drawingToolBar->getSelectedTool()->stop();
+	}
 }
+
+void DrawingBoard::handleMouseClickMotion(int x, int y)
+{	
+	if(color_panel->isClickInside(x,y))
+	{			
+		color_panel->selectClickedColorFromTriangle(x, y);			
+	}
+
+
+	if(canvas_board->isClickInside(x,y))
+	{
+		LOG("  Inside CanvasBoard");		
+		canvas_board->drawWithTool(drawingToolBar->getSelectedTool(), color_panel->getSelectedColor(), x, y);
+	}
+}
+
 void DrawingBoard::initBoard()
 {
 	canvas_board->drawBoard();
